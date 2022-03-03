54c20926e5ff7970e21f0c74e854231118c5980e
ldmnch <laiadomenechburin@gmail.com>
2022-03-03 13:53:26 -0300

    Clase 5

---
+++ M1/clase5/clase_5_practica.R
@@ -0,0 +1,16 @@
+### 1. Importar las liberías eph y tidyverse
+
+### 2. Importe la base de datos individual  del segundo trimestre de 2021.
+
+### 3. En base a la variable PP08D1, ¿cuánto cobra por mes la media de asalariados? ¿Y la mediana? 
+
+### 4. ¿Cuánto gana mensualmente el percentil 30 de los asalariados?
+### ¿Qué puede hacer con esta variable para que los outliers no afecten la respuesta?
+
+### 5. Cree un boxplot que muestre el ingreso mensual de los asalariados por región. 
+### ¿Qué puede observar a partir del gráfico? 
+
+### 6. ¿Cuál es la moda de la variable PP08D1? Responda a partir de los valores que vea en un histograma. 
+
+### 7. Cree un gráfico que cruce la variable PP08D1 por DECIFR (el decil del total de ingresos familares).
+### ¿Qué puede decir a partir de este gráfico? 
